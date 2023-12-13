f5a2359de4d15208318abe10c095fb1a2b4dd1be
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-14 01:16:45 +0300

    exceptions handled

---
+++ src/main/java/com/workintech/zoo/exceptions/ZooGlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.workintech.zoo.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ZooGlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<ZooErrorResponse> handleException(ZooException zooException){
+        log.error("ZooException occurred! Exception details: "+zooException.getMessage());
+        ZooErrorResponse zooErrorResponse = new ZooErrorResponse(zooException.getHttpStatus().value(),zooException.getMessage(),System.currentTimeMillis());
+        return new ResponseEntity<>(zooErrorResponse,zooException.getHttpStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ZooErrorResponse> handleException(Exception exception){
+        log.error("Exception occurred! Exception details: "+ exception.getMessage());
+        ZooErrorResponse zooErrorResponse = new ZooErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),exception.getMessage(),System.currentTimeMillis());
+        return new ResponseEntity<>(zooErrorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
